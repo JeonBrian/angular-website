d3f855d95cb8045d2e780b6cc4fea3e486d538e0
Brian Jeon <brian@brianjeon.com>
2018-02-12 20:04:38 -0800

    Add dummy production firebase variables. Must be replaced

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCdrKwldw2atXNEuwl7Evjbm9sNj3vTj5Y',
+    authDomain: 'angular-website-17a7c.firebaseapp.com',
+    databaseURL: 'https://angular-website-17a7c.firebaseio.com',
+    projectId: 'angular-website-17a7c',
+    storageBucket: 'angular-website-17a7c.appspot.com',
+    messagingSenderId: '579227851988'
+  }
 };
